c7d319b28d36d509f03a1c4010da23d87853a245
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-02-12 23:35:48 +0530

    add reusable component

--- namastebites/app/explore/page.tsx
+++ namastebites/app/explore/page.tsx
@@ -1,7 +1,35 @@
+import Items from "../components/items"
+import { Item } from "../types/types";
+import "./explore.css"
 export default () => {
+  const items: Item[] = [];
+  for (let i = 0; i < 5; i++) {
+    items.push({
+      id: i.toString(),
+      name: "Item " + i,
+      price: i * 10,
+      url: 'https://picsum.photos/seed/1234/400/400'
+    })
+  }
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className="explore-container">
+      <div className="explore">
+        <div className="explore-header">
+          <h1 className="explore-text">Explore</h1>
+        </div>
+        <div className="explore-search">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="explore-body">
+          <Items items={items} Component={({ item }) => {
+            return (
+              <div>
+                {item.name}
+              </div>
+            )
+          }} />
+        </div>
+      </div>
     </div>
   )
 }
