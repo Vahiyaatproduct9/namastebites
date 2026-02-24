6f8327b0c082d063fba6f0fc9b575fa452fb4db9
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-02-25 00:50:37 +0530

    confirm hotfix

--- namastebites/app/cart/page.tsx
+++ namastebites/app/cart/page.tsx
@@ -1,39 +1,49 @@
-'use client';
+"use client";
 import React from "react";
-import "./cart.css"
-import { useCart } from "@store/useCart"
-export default () => {
-  const cart = useCart(s => s.cart);
-  const getTotalPrice = useCart(s => s.getTotalPrice);
-  const increaseQuantity = useCart(s => s.addToCart);
-  const decreaseQuantity = useCart(s => s.decreaseQuantity);
-  const CartBody = () => cart.map(t => {
-    const incrementItem = (id: string, e: React.MouseEvent<HTMLButtonElement>) => {
-      const item = cart.find(t => t.id === id)!;
-      e.preventDefault();
-      increaseQuantity(item);
-    }
-    const decrementItem = (id: string, e: React.MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      decreaseQuantity(id);
-    }
-    return (
-      <div key={t.id} className="item-body">
-        <div className="thumbnail" style={{ backgroundImage: `url(${t.url})` }} />
-        <div className="item-details">
-          <div className="details">
-            <div className="item-name">{t.name}</div>
-            <div className="item-price">₹{t.price * t.quantity}</div>
-          </div>
-          <div className="quantity">
-            <button onClick={e => decrementItem(t.id, e)}>-</button>
-            <div>{t.quantity}</div>
-            <button onClick={e => incrementItem(t.id, e)}>+</button>
+import "./cart.css";
+import { useCart } from "@store/useCart";
+const Cart = () => {
+  const cart = useCart((s) => s.cart);
+  const getTotalPrice = useCart((s) => s.getTotalPrice);
+  const increaseQuantity = useCart((s) => s.addToCart);
+  const decreaseQuantity = useCart((s) => s.decreaseQuantity);
+  const CartBody = () =>
+    cart.map((t) => {
+      const incrementItem = (
+        id: string,
+        e: React.MouseEvent<HTMLButtonElement>,
+      ) => {
+        const item = cart.find((t) => t.id === id)!;
+        e.preventDefault();
+        increaseQuantity(item);
+      };
+      const decrementItem = (
+        id: string,
+        e: React.MouseEvent<HTMLButtonElement>,
+      ) => {
+        e.preventDefault();
+        decreaseQuantity(id);
+      };
+      return (
+        <div key={t.id} className="item-body">
+          <div
+            className="thumbnail"
+            style={{ backgroundImage: `url(${t.url})` }}
+          />
+          <div className="item-details">
+            <div className="details">
+              <div className="item-name">{t.name}</div>
+              <div className="item-price">₹{t.price * t.quantity}</div>
+            </div>
+            <div className="quantity">
+              <button onClick={(e) => decrementItem(t.id, e)}>-</button>
+              <div>{t.quantity}</div>
+              <button onClick={(e) => incrementItem(t.id, e)}>+</button>
+            </div>
           </div>
         </div>
-      </div>
-    )
-  })
+      );
+    });
   return (
     <div className="cart">
       <div className="flex-body">
@@ -46,17 +56,17 @@ export default () => {
               <CartBody />
             </div>
           </div>
-          <div className="cart-footer">
-          </div>
+          <div className="cart-footer"></div>
         </div>
         <div className="body-2">
           <div className="checkout">
-            <div className="">
-
-            </div>
+            <div className=""></div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Cart;
+
