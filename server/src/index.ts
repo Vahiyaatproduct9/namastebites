41f3f286387eb269e0f2796a76e17024bf715b41
Kishor <kishor@usepiston.com>
2026-02-24 23:41:48 +0530

    connect backend and update project

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,52 @@ const port = process.env.PORT || 8000;
 import exploreRouter from "@router/explore.router";
 import paymentRouter from "@router/payment.router";
 import cors from "@elysiajs/cors";
-import { errorHandler } from "./middleware/error-handler";
+import { ZodError } from "zod";
+import { DatabaseError } from "pg";
 const app = new Elysia()
-  .onError(errorHandler)
+  .onRequest((ctx) => {
+    console.log("ctx body:", JSON.stringify(ctx.request.body, null, 2));
+    console.log("ctx method:", ctx.request.method);
+  })
+  .onError((err) => {
+    const error = err.error;
+    console.error("You have an error: ", error);
+    if (error instanceof ZodError) {
+      err.set.status = 400;
+      return {
+        status: 400,
+        message: "Invalid Input Data.",
+      };
+    }
+    if (error instanceof DatabaseError) {
+      return {
+        status: 500,
+        message: "Whoops! Our Database is down.",
+      };
+    }
+    return {
+      status: 500,
+      message: "Internal Server Error",
+    };
+  })
+  .mapResponse((ctx) => {
+    let response = ctx.responseValue as any;
+    ctx.set.status = response?.status;
+    if (response?.status && response.status >= 400) {
+      response = {
+        ...response,
+        success: response?.success ?? false,
+      };
+    }
+    response = {
+      ...response,
+      success: response?.success ?? true,
+    };
+    return {
+      ...response,
+      provider: "Namaste Bites :)",
+    };
+  })
   .use(
     cors({
       origin: "*",
