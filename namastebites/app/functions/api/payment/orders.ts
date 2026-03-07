22bc3a9beb241674e9cd357e89485401ec7cd9a3
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-03-07 12:22:23 +0530

    .

--- namastebites/app/functions/api/payment/orders.ts
+++ namastebites/app/functions/api/payment/orders.ts
@@ -1,32 +1,20 @@
 import path from "@/app/path/path";
 import { CartItem } from "@/app/types/types";
 
-export default class Order {
-  private orders: CartItem[];
-  constructor(orders: CartItem[]) {
-    this.orders = orders;
-  }
-  /**
-   * This method is used to initiate the payment for the order.
-   */
-  public async initiatePayment() {
-    const response = await fetch(`${path}/payment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(this.orders),
-    });
-    if (!response.ok) {
-      throw new Error("Failed to create order");
-    }
-    const data = await response.json();
-    return data;
-  }
-  /**
-   * This method is used to get the payment status for the order.
-   */
-  public async getPaymentStatus() {
-    return true;
-  }
+async function initiatePayment(items: CartItem[]) {
+  const response = await fetch(`${path}/payment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(items),
+  });
+  const result = await response.json();
+  return result;
 }
+
+const order = {
+  initiatePayment,
+};
+
+export default order;
