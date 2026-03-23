eec1994f2c61d379a8f04f2df19ca0680a056d0c
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-03-23 14:18:02 +0530

    debug commit

--- namastebites/app/path/path.ts
+++ namastebites/app/path/path.ts
@@ -1,2 +1,4 @@
 // export default "http://localhost:8000";
-export default process.env.NEXT_PUBLIC_BACKEND_URL || undefined;
+export default "https://katelynn-unstrategic-unsensitively.ngrok-free.dev" ||
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  undefined;
