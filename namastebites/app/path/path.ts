f7b1adf62fc2d8921e5ca4f72e820bf7b3dc9a8d
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-03-23 14:21:10 +0530

    debug commit

--- namastebites/app/path/path.ts
+++ namastebites/app/path/path.ts
@@ -1,4 +1,5 @@
 // export default "http://localhost:8000";
-export default "https://katelynn-unstrategic-unsensitively.ngrok-free.dev" ||
-  process.env.NEXT_PUBLIC_BACKEND_URL ||
-  undefined;
+export default "https://katelynn-unstrategic-unsensitively.ngrok-free.dev";
+// ||
+// process.env.NEXT_PUBLIC_BACKEND_URL ||
+// undefined;
